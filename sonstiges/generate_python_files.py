4c3e709925483aa0530e3bb34fbb9ae85b2b36b4
John Doe <johndoe@example.com>
2024-06-10 17:32:56 +0200

    update generate_python_files.py

---
+++ sonstiges/generate_python_files.py
@@ -0,0 +1,7 @@
+for i in range(1, 8):
+    if i == 1:
+        open(f"entwurf_mein_program_version_{i}_06_2024.py", "w")
+    elif i == 7:
+        open(f"fertig_mein_program_version_{i}_06_2024.py", "w")
+    else:
+        open(f"mein_program_version_{i}_06_2024.py", "w")
